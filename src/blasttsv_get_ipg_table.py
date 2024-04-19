90baa3952abf19bc8827f3475b3dd6e5d978144a
Paul J. Kreymborg <paul.kreymborg@princeton.edu>
2024-04-19 15:39:28 -0400

    add back print

--- src/blasttsv_get_ipg_table.py
+++ src/blasttsv_get_ipg_table.py
@@ -13,8 +13,8 @@ def search_proteins_in_entrez(all_prot_accession_ids):
         prot_search_group = all_prot_accession_ids[i * group_search_size : (i + 1) * group_search_size]
         http_response = Entrez.esearch("protein", ",".join(prot_search_group))
         text_response = http_response.read().decode('utf-8')
-        # print(prot_search_group)
-        # print(text_response)
+        print(prot_search_group)
+        print(text_response)
 
         xml_resp = xml.etree.ElementTree.fromstring(text_response)
         # xml_resp.getroot().
