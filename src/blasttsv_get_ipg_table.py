480c958c983272c9e68e0139c1fdb9acc026084b
Paul J. Kreymborg <paul.kreymborg@princeton.edu>
2024-04-19 14:12:29 -0400

    try this stuf

--- src/blasttsv_get_ipg_table.py
+++ src/blasttsv_get_ipg_table.py
@@ -1,5 +1,6 @@
 import argparse
 # import pandas as pd
+import subprocess
 # "/home/pk5192/Documents/blast_searching_slurm/data"
 
 if __name__ == "__main__":
@@ -8,12 +9,17 @@ if __name__ == "__main__":
                     description="Take the tsv output from the blast and use eutils to get source info"
     )
     parser.add_argument("source_blast_tsv")
+    parser.add_argument("intermediate_prot_accession_file")
     parser.add_argument("output_efetch_tsv")
     args = parser.parse_args()
     if args.source_blast_tsv[-4:] != ".tsv":
         raise Exception("Expected tsv file!")
+    if args.intermediate_prot_accession_file[-4:] != ".txt":
+        raise Exception("expected txt file!")
     elif args.output_efetch_tsv[-4:] != ".tsv":
         raise Exception("expected tsv file")
+    
+    prot_accessions_to_search = []
     
     with open(args.source_blast_tsv, "r") as f:
         for line in f:
@@ -23,6 +29,31 @@ if __name__ == "__main__":
             
             refs_for_protseq_match = line_vals[3]
             for protref in refs_for_protseq_match.split(";"):
-                print(protref.split("|")[1])
+                prot_accession = protref.split("|")[1]
+                # print(prot_accession)
+                prot_accessions_to_search.append(prot_accessions_to_search)
             # print(refs_for_protseq_match.split(";"))
             break
+    with open(args.intermediate_prot_accession_file, "w") as f:
+        f.write(",".join(prot_accessions_to_search))
+    
+    comm = "cat '{accession_text_list}' | epost -db protein | efetch -format ipg  > {output_efetch_tsv}".format(
+        accession_text_list=args.intermediate_prot_accession_file,
+        output_efetch_tsv=output_efetch_tsv,
+        )
+
+    process = subprocess.Popen(comm,shell=True,stdin=subprocess.PIPE,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+
+    output, error=process.communicate()
+    errcode = process.returncode
+    
+    output = output.decode("utf-8")
+    error = error.decode("utf-8")
+
+    if errcode != 0:
+        print(output)
+        print(error)
+        raise Exception("Got error code {}".format(errcode))
+    
+    print(output)
+    print(error)
