e31a857125efcb071d915b88a312f3fd4bf7bdaf
Paul J. Kreymborg <paul.kreymborg@princeton.edu>
2024-04-19 15:32:25 -0400

    try this

--- src/blasttsv_get_ipg_table.py
+++ src/blasttsv_get_ipg_table.py
@@ -9,7 +9,7 @@ def search_proteins_in_entrez(all_prot_accession_ids):
     group_search_size = 20
     for i in range(math.ceil(len(all_prot_accession_ids) / 1000)):
         prot_search_group = all_prot_accession_ids[i * group_search_size : (i + 1) * group_search_size]
-        http_response = Entrez.esearch("protein", ",".join(prot_search_group), rettype="gb")
+        http_response = Entrez.esearch("protein", ",".join(prot_search_group), rettype="text")
         text_response = http_response.read().decode('utf-8')
         print(text_response)
 
