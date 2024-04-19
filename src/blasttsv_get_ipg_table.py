9cef14c60ea6e9eebaf99d6803bb8883f1c1fd63
Paul J. Kreymborg <paul.kreymborg@princeton.edu>
2024-04-19 16:14:27 -0400

    remove cap

--- src/blasttsv_get_ipg_table.py
+++ src/blasttsv_get_ipg_table.py
@@ -42,7 +42,7 @@ def search_proteins_in_entrez(all_prot_accession_ids):
 
 
 def get_protein_info_from_entrez(prot_accessions):
-    group_search_size = 100
+    group_search_size = 500
     output_tsv_string = ""
     for i in range(math.ceil(len(prot_accessions) / group_search_size)):
         start_idx = i * group_search_size
@@ -123,7 +123,7 @@ if __name__ == "__main__":
             # prot_accessions_to_search
             # print(refs_for_protseq_match.split(";"))
             # break
-    prot_accessions_to_search = prot_accessions_to_search[0:120]
+    # prot_accessions_to_search = prot_accessions_to_search[0:120]
     print("Searching entrez for {} protein accessions".format(len(prot_accessions_to_search)))
     prot_accession_presentindb = search_proteins_in_entrez(prot_accessions_to_search)
     print("Found {} accessions through entrez, continuing with those".format(len(prot_accession_presentindb)))
