7e4e7603b2b67c0620ecbc37b5aa93f36cf590cc
Paul J. Kreymborg <paul.kreymborg@princeton.edu>
2024-04-20 16:47:02 -0400

    less threads

--- src/run_genomespot_on_genomes.py
+++ src/run_genomespot_on_genomes.py
@@ -124,11 +124,11 @@ def run_genomespot_slurmjobs(genome_and_proteins_and_save_infos, genomespot_mode
     # 10 minute jobs with 30 genomes each
 
     genomes_per_job = 1000
-    nthreads = 20
+    nthreads = 5
     # each batch will take about 6 seconds, so give some leeway
     # (genomes_per_job/nthreads) * 6 seconds * safety factor
     # Try to give more time actually
-    timestr_per_job = "00:40:00"
+    timestr_per_job = "01:20:00"
     jobmemory="8G" # Need about 500 mb per thread
 
     n_jobs = math.ceil(len(genome_and_proteins_and_save_infos) / genomes_per_job)
