81630fed91d7f698f15d2ccbc248a96782cb08a3
Paul J. Kreymborg <paul.kreymborg@princeton.edu>
2024-04-20 10:21:47 -0400

    trying out this

---
+++ src/download_genome_fastas.py
@@ -0,0 +1,36 @@
+# from Bio import Entrez
+import argparse
+import requests
+import pandas as pd
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        prog="get_ftp_links_for_genomes",
+        description="Get ftp links from tsv summary of the sequences",
+    )
+    parser.add_argument("source_summary_tsv")
+    parser.add_argument("genome_data_dir")
+
+    args = parser.parse_args()
+    if not args.source_summary_tsv.endswith(".tsv"):
+        raise Exception("Expected tsv file here")
+
+
+
+    os.makedirs(args.genome_data_dir, exist_ok=True)
+
+    df = pd.read_csv(args.source_summary_tsv, sep="\t")
+
+    for idx, row in df.iterrows():
+        genome_accession_id = row["Assembly"]
+
+        request_url = (r"https://api.ncbi.nlm.nih.gov/datasets/v2alpha/genome/accession/" +
+            r"{}/download?include_annotation_type=GENOME_FASTA&include_annotation_type=GENOME_GFF&".format(genome_accession_id) +
+            r"include_annotation_type=RNA_FASTA&include_annotation_type=CDS_FASTA&include_annotation_type=PROT_FASTA&"+
+            r"include_annotation_type=SEQUENCE_REPORT&hydrated=FULLY_HYDRATED")
+
+
+        r = requests.get(request_url)
+
+        break
+
